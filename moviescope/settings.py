bbd45d29754e7b67aef65ef5519132055ea81caf
Aakash Sethi <aakashsethi20@gmail.com>
2016-09-23 14:49:40 -0400

    project added. Fix bootstrap to deploy on Heroku

--- moviescope/settings.py
+++ moviescope/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gkebr89@+3*74a+y6aputp50&f1y1+bk^md$14@^dutnkh78a6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
